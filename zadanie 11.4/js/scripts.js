acb52827f869b8896d30740b873feef629660b58
Michał Dolny <michal.dolny@getresponse.com>
2016-12-23 17:48:44 +0100

    wciecie

--- zadanie 11.4/js/scripts.js
+++ zadanie 11.4/js/scripts.js
@@ -1,7 +1,7 @@
 var url = 'http://api.icndb.com/jokes/random';
 
 var button = document.getElementById('get-joke');
-	button.addEventListener('click', function(){
+button.addEventListener('click', function(){
   	getJoke();
 });
 
