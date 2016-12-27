e8e85b82d91383c8ddcc61ed7861f1027a4a37b8
Michał Dolny <michal.dolny@getresponse.com>
2016-12-27 22:23:54 +0100

    jedna petla za duzo

--- zadanie 11.6/js/scripts.js
+++ zadanie 11.6/js/scripts.js
@@ -16,8 +16,6 @@ function searchCountries() {
 function showCountriesList(resp) {
   	countriesList.empty();
     resp.forEach(function(item) {
-	     resp.forEach(function(item){
-   		 $('<li>').text(item.name).appendTo(countriesList);
-	   });
-  });
+   	    $('<li>').text(item.name).appendTo(countriesList);
+    });
 }
